fcb1bfe9951da800dd7e0b0d3466e3c8d104bd9c
Ryzzzo <r.stacy@me.com>
2025-12-05 00:58:22 -0500

    Updated SpaceBackground.tsx

--- components/vx/SpaceBackground.tsx
+++ components/vx/SpaceBackground.tsx
@@ -33,31 +33,22 @@ export default function SpaceBackground() {
 
     let currentGradientX = 0;
     let currentGradientY = 0;
-    let currentParticlesX = 0;
-    let currentParticlesY = 0;
     let currentStarfieldX = 0;
     let currentStarfieldY = 0;
 
     const animate = () => {
       const targetGradientX = -mousePositionRef.current.normalizedX * 5;
       const targetGradientY = -mousePositionRef.current.normalizedY * 5;
-      const targetParticlesX = -mousePositionRef.current.normalizedX * 15;
-      const targetParticlesY = -mousePositionRef.current.normalizedY * 15;
       const targetStarfieldX = -mousePositionRef.current.normalizedX * 10;
       const targetStarfieldY = -mousePositionRef.current.normalizedY * 10;
 
       currentGradientX += (targetGradientX - currentGradientX) * 0.1;
       currentGradientY += (targetGradientY - currentGradientY) * 0.1;
-      currentParticlesX += (targetParticlesX - currentParticlesX) * 0.1;
-      currentParticlesY += (targetParticlesY - currentParticlesY) * 0.1;
       currentStarfieldX += (targetStarfieldX - currentStarfieldX) * 0.1;
       currentStarfieldY += (targetStarfieldY - currentStarfieldY) * 0.1;
 
       if (gradientRef.current) {
         gradientRef.current.style.transform = `translate3d(${currentGradientX}px, ${currentGradientY}px, 0)`;
-      }
-      if (particlesRef.current) {
-        particlesRef.current.style.transform = `translate3d(${currentParticlesX}px, ${currentParticlesY}px, 0)`;
       }
       if (starfieldRef.current) {
         starfieldRef.current.style.transform = `translate3d(${currentStarfieldX}px, ${currentStarfieldY}px, 0)`;
@@ -163,7 +154,6 @@ export default function SpaceBackground() {
       <div
         ref={particlesRef}
         className="absolute inset-0 z-0"
-        style={{ willChange: 'transform' }}
       >
         <Particles
           id="space-particles"
