54dcff32bfbcbd1963476e5c4d666c3981799975
Ryzzzo <r.stacy@me.com>
2025-12-08 22:07:38 -0500

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -199,6 +199,15 @@ export default function RootLayout({
             gtag('config', 'G-JMBHGHJWGY');
           `}
         </Script>
+        <Script
+          id="chatbase-config"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="DxHbjHt8UTb0OXB7-Kawu";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
+            `
+          }}
+        />
       </body>
     </html>
   );
